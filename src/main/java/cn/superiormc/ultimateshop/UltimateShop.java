9f2d2b2235d610d53b9a47605b34cff6187c75be
PQguanfang <1686965528@qq.com>
2023-08-30 23:22:04 +0800

    Initial commit

---
+++ src/main/java/cn/superiormc/ultimateshop/UltimateShop.java
@@ -0,0 +1,17 @@
+package cn.superiormc.ultimateshop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UltimateShop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
