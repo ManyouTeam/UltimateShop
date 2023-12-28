b29b16845c6939b950f23b8f5ceac915a097462c
PQguanfang <pqguanfang@foxmail.com>
2023-12-28 12:08:41 +0800

    rollback change

--- src/main/java/cn/superiormc/ultimateshop/gui/inv/ShopGUI.java
+++ src/main/java/cn/superiormc/ultimateshop/gui/inv/ShopGUI.java
@@ -42,7 +42,6 @@ public class ShopGUI extends InvGUI {
     @Override
     public void openGUI() {
         super.openGUI();
-        constructGUI();
         if (ConfigManager.configManager.getBoolean("menu.shop.update")) {
             runTask = new BukkitRunnable() {
                 @Override
@@ -51,23 +50,6 @@ public class ShopGUI extends InvGUI {
                 }
             };
             runTask.runTaskTimer(UltimateShop.instance, 20L, 20L);
-        }
-        shopMenu = ObjectMenu.shopMenus.get(shop);
-        if (shopMenu == null) {
-            LanguageManager.languageManager.sendStringText(owner.getPlayer(),
-                    "error.shop-menu-not-found",
-                    "shop",
-                    shop.getShopName(),
-                    "menu",
-                    shop.getShopMenu());
-            return;
-        }
-        if (!shopMenu.getCondition().getBoolean(owner.getPlayer())) {
-            LanguageManager.languageManager.sendStringText(owner,
-                    "menu-condition-not-meet",
-                    "menu",
-                    shop.getShopMenu());
-            return;
         }
     }
 
@@ -87,6 +69,23 @@ public class ShopGUI extends InvGUI {
                     "error.shop-does-not-have-menu",
                     "shop",
                     shop.getShopName());
+            return;
+        }
+        shopMenu = ObjectMenu.shopMenus.get(shop);
+        if (shopMenu == null) {
+            LanguageManager.languageManager.sendStringText(owner.getPlayer(),
+                    "error.shop-menu-not-found",
+                    "shop",
+                    shop.getShopName(),
+                    "menu",
+                    shop.getShopMenu());
+            return;
+        }
+        if (!shopMenu.getCondition().getBoolean(owner.getPlayer())) {
+            LanguageManager.languageManager.sendStringText(owner,
+                    "menu-condition-not-meet",
+                    "menu",
+                    shop.getShopMenu());
             return;
         }
         for (ObjectItem tempVal5 : shop.getProductList()) {
--- src/main/java/cn/superiormc/ultimateshop/listeners/GUIListener.java
+++ src/main/java/cn/superiormc/ultimateshop/listeners/GUIListener.java
@@ -45,6 +45,7 @@ public class GUIListener implements Listener {
             }
         }
         catch (Exception ep) {
+            ep.printStackTrace();
             e.setCancelled(true);
         }
     }
