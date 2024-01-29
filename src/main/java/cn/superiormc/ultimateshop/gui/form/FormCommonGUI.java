02586f8eaf11c479bfe5bd30404d2aa10b95f83f
PQguanfang <pqguanfang@foxmail.com>
2024-01-29 10:43:26 +0800

    1.10.5

--- src/main/java/cn/superiormc/ultimateshop/gui/form/FormCommonGUI.java
+++ src/main/java/cn/superiormc/ultimateshop/gui/form/FormCommonGUI.java
@@ -2,6 +2,7 @@ package cn.superiormc.ultimateshop.gui.form;
 
 import cn.superiormc.ultimateshop.gui.FormGUI;
 import cn.superiormc.ultimateshop.managers.LanguageManager;
+import cn.superiormc.ultimateshop.objects.buttons.AbstractButton;
 import cn.superiormc.ultimateshop.objects.menus.ObjectMenu;
 import cn.superiormc.ultimateshop.utils.CommonUtil;
 import cn.superiormc.ultimateshop.utils.TextUtil;
@@ -9,6 +10,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.geysermc.cumulus.component.ButtonComponent;
 import org.geysermc.cumulus.form.SimpleForm;
+import org.geysermc.cumulus.util.FormImage;
 
 import java.util.Objects;
 
@@ -38,15 +40,29 @@ public class FormCommonGUI extends FormGUI {
         SimpleForm.Builder tempVal2 = SimpleForm.builder();
         if (Objects.isNull(form)) {
             for (int slot : menuButtons.keySet()) {
+                AbstractButton button = menuButtons.get(slot);
                 String tempVal3 = TextUtil.parse(
-                        CommonUtil.getItemName(menuButtons.get(slot).getDisplayItem(
+                        CommonUtil.getItemName(button.getDisplayItem(
                                 owner.getPlayer(), 1)));
                 if (tempVal3.length() == 0) {
                     continue;
                 }
-                ButtonComponent tempVal1 = ButtonComponent.of(tempVal3);
-                tempVal2.button(tempVal1);
-                menuItems.put(tempVal1, slot);
+                ButtonComponent tempVal6 = null;
+                String icon = button.getButtonConfig().getString("bedrock-icon");
+                if (icon != null && icon.split(";;").length == 2) {
+                    String type = icon.split(";;")[0].toLowerCase();
+                    if (type.equals("url")) {
+                        tempVal6 = ButtonComponent.of(tempVal3, FormImage.Type.URL, icon.split(";;")[1]);
+                    } else if (type.equals("path")) {
+                        tempVal6 = ButtonComponent.of(tempVal3, FormImage.Type.PATH, icon.split(";;")[1]);
+                    }
+                } else {
+                    tempVal6 = ButtonComponent.of(tempVal3);
+                }
+                if (tempVal6 != null) {
+                    tempVal2.button(tempVal6);
+                }
+                menuItems.put(tempVal6, slot);
             }
         }
         tempVal2.title(TextUtil.parse(commonMenu.getString("title", "Shop")));
--- src/main/java/cn/superiormc/ultimateshop/gui/form/FormShopGUI.java
+++ src/main/java/cn/superiormc/ultimateshop/gui/form/FormShopGUI.java
@@ -7,6 +7,7 @@ import cn.superiormc.ultimateshop.managers.CacheManager;
 import cn.superiormc.ultimateshop.managers.ConfigManager;
 import cn.superiormc.ultimateshop.managers.LanguageManager;
 import cn.superiormc.ultimateshop.objects.ObjectShop;
+import cn.superiormc.ultimateshop.objects.buttons.AbstractButton;
 import cn.superiormc.ultimateshop.objects.buttons.ObjectItem;
 import cn.superiormc.ultimateshop.objects.caches.ObjectUseTimesCache;
 import cn.superiormc.ultimateshop.objects.menus.ObjectMenu;
@@ -15,9 +16,9 @@ import cn.superiormc.ultimateshop.utils.TextUtil;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.geysermc.cumulus.component.ButtonComponent;
 import org.geysermc.cumulus.form.SimpleForm;
+import org.geysermc.cumulus.util.FormImage;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -66,16 +67,14 @@ public class FormShopGUI extends FormGUI {
             ObjectUseTimesCache tempVal3 = tempVal1.getUseTimesCache().get(tempVal5);
             if (tempVal3 != null && tempVal3.getBuyRefreshTime() != null && tempVal3.getBuyRefreshTime().isBefore(LocalDateTime.now())) {
                 if (ConfigManager.configManager.getBoolean("debug")) {
-                    Bukkit.getConsoleSender().sendMessage("" +
-                            "§x§9§8§F§B§9§8[UltimateShop] §bReset player buy data by GUI open check!");
+                    Bukkit.getConsoleSender().sendMessage("§x§9§8§F§B§9§8[UltimateShop] §bReset player buy data by GUI open check!");
                 }
                 tempVal1.getUseTimesCache().get(tempVal5).setBuyUseTimes(0);
                 tempVal1.getUseTimesCache().get(tempVal5).setLastBuyTime(null);
             }
             if (tempVal3 != null && tempVal3.getSellRefreshTime() != null && tempVal3.getSellRefreshTime().isBefore(LocalDateTime.now())) {
                 if (ConfigManager.configManager.getBoolean("debug")) {
-                    Bukkit.getConsoleSender().sendMessage("" +
-                            "§x§9§8§F§B§9§8[UltimateShop] §bReset player sell data by GUI open check!");
+                    Bukkit.getConsoleSender().sendMessage("§x§9§8§F§B§9§8[UltimateShop] §bReset player sell data by GUI open check!");
                 }
                 tempVal1.getUseTimesCache().get(tempVal5).setSellUseTimes(0);
                 tempVal1.getUseTimesCache().get(tempVal5).setLastSellTime(null);
@@ -83,16 +82,14 @@ public class FormShopGUI extends FormGUI {
             ObjectUseTimesCache tempVal4 = tempVal2.getUseTimesCache().get(tempVal5);
             if (tempVal4 != null && tempVal4.getBuyRefreshTime() != null && tempVal4.getBuyRefreshTime().isBefore(LocalDateTime.now())) {
                 if (ConfigManager.configManager.getBoolean("debug")) {
-                    Bukkit.getConsoleSender().sendMessage("" +
-                            "§x§9§8§F§B§9§8[UltimateShop] §bReset server buy data by GUI open check!");
+                    Bukkit.getConsoleSender().sendMessage("§x§9§8§F§B§9§8[UltimateShop] §bReset server buy data by GUI open check!");
                 }
                 tempVal2.getUseTimesCache().get(tempVal5).setBuyUseTimes(0);
                 tempVal2.getUseTimesCache().get(tempVal5).setLastBuyTime(null);
             }
             if (tempVal4 != null && tempVal4.getSellRefreshTime() != null && tempVal4.getSellRefreshTime().isBefore(LocalDateTime.now())) {
                 if (ConfigManager.configManager.getBoolean("debug")) {
-                    Bukkit.getConsoleSender().sendMessage("" +
-                            "§x§9§8§F§B§9§8[UltimateShop] §bReset server sell data by GUI open check!");
+                    Bukkit.getConsoleSender().sendMessage("§x§9§8§F§B§9§8[UltimateShop] §bReset server sell data by GUI open check!");
                 }
                 tempVal2.getUseTimesCache().get(tempVal5).setSellUseTimes(0);
                 tempVal2.getUseTimesCache().get(tempVal5).setLastSellTime(null);
@@ -107,14 +104,28 @@ public class FormShopGUI extends FormGUI {
         SimpleForm.Builder tempVal5 = SimpleForm.builder();
         if (Objects.isNull(form)) {
             for (int slot : menuButtons.keySet()) {
+                AbstractButton button = menuButtons.get(slot);
                 String tempVal3 = TextUtil.parse(
-                        CommonUtil.getItemName(menuButtons.get(slot).getDisplayItem(
+                        CommonUtil.getItemName(button.getDisplayItem(
                                 owner.getPlayer(), 1)));
-                if (tempVal3.length() == 0) {
+                if (tempVal3.trim().length() == 0) {
                     continue;
                 }
-                ButtonComponent tempVal6 = ButtonComponent.of(tempVal3);
-                tempVal5.button(tempVal6);
+                ButtonComponent tempVal6 = null;
+                String icon = button.getButtonConfig().getString("bedrock-icon");
+                if (icon != null && icon.split(";;").length == 2) {
+                    String type = icon.split(";;")[0].toLowerCase();
+                    if (type.equals("url")) {
+                        tempVal6 = ButtonComponent.of(tempVal3, FormImage.Type.URL, icon.split(";;")[1]);
+                    } else if (type.equals("path")) {
+                        tempVal6 = ButtonComponent.of(tempVal3, FormImage.Type.PATH, icon.split(";;")[1]);
+                    }
+                } else {
+                    tempVal6 = ButtonComponent.of(tempVal3);
+                }
+                if (tempVal6 != null) {
+                    tempVal5.button(tempVal6);
+                }
                 menuItems.put(tempVal6, slot);
             }
         }
